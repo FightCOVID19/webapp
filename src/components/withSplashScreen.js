ca173a6dcd5a4a6cf1191ea528eab2451961515a
Camilo Pestana <capcarde@gmail.com>
2020-03-26 16:05:07 +0800

    Added ScreenSplash and styles for Login

---
+++ src/components/withSplashScreen.js
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+function LoadingMessage() {
+  return (
+    <div className="ai-login__splashscreen">
+      <img src='logo.png' width="200px" height="200px"/>
+    </div>
+  );
+}
+
+function withSplashScreen(WrappedComponent) {
+  return class extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: true,
+      };
+    }
+
+    async componentDidMount() {
+      try {
+        //await auth0Client.loadSession();
+        // here we should await login
+        setTimeout(() => {
+          this.setState({
+            loading: false,
+          });
+        }, 1500)
+      } catch (err) {
+        console.log(err);
+        this.setState({
+          loading: false,
+        });
+      }
+    }
+
+    render() {
+      // while checking user session, show "loading" message
+      if (this.state.loading) return LoadingMessage();
+
+      // otherwise, show the desired route
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+}
+
+export default withSplashScreen;
